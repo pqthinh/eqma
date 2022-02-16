a2675cec04735d9a215a1f164402c48c3227c203
pqthinh <18021231@vnu.edu.vn>
2022-02-17 02:25:15 +0700

    fake dashboard

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,8 +1,16 @@
+import { IMAGES } from 'assets'
+import { WrapperContentBody } from 'organisms'
 import React from 'react'
+import { Constants } from 'utils'
+import { Image, WrapperContent } from './styled'
 
 function Dashboard() {
   return (
-    <div>index</div>
+    <WrapperContentBody top={null} contentBody={Constants.contentPage[1].title}>
+      <WrapperContent>
+        <Image source={IMAGES.NO_CONTENT.default} />
+      </WrapperContent>
+    </WrapperContentBody>
   )
 }
 
