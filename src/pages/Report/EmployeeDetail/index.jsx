8e7168bf127408ab432f36443c8b8ecf9b9f028b
pqthinh <18021231@vnu.edu.vn>
2022-03-19 02:45:37 +0700

    emp detail base

--- src/pages/Report/EmployeeDetail/index.jsx
+++ src/pages/Report/EmployeeDetail/index.jsx
@@ -1,15 +1,40 @@
-import { IMAGES } from 'assets'
-import React, { useCallback, useMemo, useState } from 'react'
+import React, { useCallback, useState, useEffect } from 'react'
 import NotFoundPage from '../../NotFoundPage'
-import { ContentBody, Image, Content } from './styled'
+import { Content, ContentBody } from './styled'
+import { useRequestManager } from 'hooks'
+import { withArray, withObject } from 'exp-value'
+import { EndPoint } from 'config/api'
 
 const EmployeeDetail = () => {
   const [activeKeyNav, setActiveKeyNav] = useState('1')
+  const [info, setInfo] = useState()
+  const [request, setRequest] = useState()
+  const [liquidation, setLiquidation] = useState()
+  const [repair, setRepair] = useState()
+  const [equiment_log, setEquiment_log] = useState()
+
+  const { onGetExecute } = useRequestManager()
+
   const onSelect = useCallback(e => setActiveKeyNav(e), [activeKeyNav])
 
-  const renderTimeline = useMemo(() => {
-    return <Image source={IMAGES.NO_CONTENT.default} />
+  const getEmp = useCallback(params => {
+    async function execute(params) {
+      const result = await onGetExecute(EndPoint.GET_LIST_EMP, {
+        params: params
+      })
+      if (result) {
+        setInfo(withObject('data', result))
+        setRequest(withArray('data.request', result))
+        setRepair(withArray('data.repair', result))
+        setLiquidation(withArray('data.liquidation', result))
+        setEquiment_log(withArray('data.equipment_status_log', result))
+      }
+    }
+    console.log(params, 'params')
+    execute(params)
   }, [])
+
+  useEffect(getEmp, [])
 
   const _renderContentPage = useCallback(() => {
     if (activeKeyNav == '1') {
@@ -18,24 +43,23 @@ const EmployeeDetail = () => {
     if (activeKeyNav == '2') {
       return null
     }
-    if (activeKeyNav == '3') {
-      return null
-    }
-    if (activeKeyNav == '4') {
-      return renderTimeline
-    }
     return <NotFoundPage />
   }, [activeKeyNav])
 
+  const InfoEmp = () => {
+    return <p>Thinh</p>
+  }
+
   return (
     <ContentBody
-      contentBody={"Thông tin nhân viên"}
+      contentBody={'Thông tin nhân viên'}
       items={[
         { contentName: 'Lịch sử yêu cầu', iconName: 'feather-gift' },
-        { contentName: 'Lịch sử mượn trả', iconName: 'feather-tool' },
+        { contentName: 'Lịch sử mượn trả', iconName: 'feather-tool' }
       ]}
       activeKey={activeKeyNav}
       setActiveKey={onSelect}
+      top={<InfoEmp />}
     >
       <Content>{_renderContentPage()}</Content>
     </ContentBody>
