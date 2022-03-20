7a5dc57da42002a5e2dbbbb86b934dcfafb71372
pqthinh <18021231@vnu.edu.vn>
2022-03-20 11:12:10 +0700

    get equiment

--- src/components/molecules/Form/EmployeeRequest/index.jsx
+++ src/components/molecules/Form/EmployeeRequest/index.jsx
@@ -1,8 +1,8 @@
 import { EndPoint } from 'config/api'
-import { withEmpty } from 'exp-value'
+import { withEmpty, withArray } from 'exp-value'
 import { useAlert, useRequestManager } from 'hooks'
 import PropTypes from 'prop-types'
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useState,useEffect } from 'react'
 import { SelectPicker } from 'rsuite'
 import { Constants } from 'utils'
 import InputGroup from '../../InputGroup'
@@ -11,8 +11,9 @@ import { employeeRequestModel } from './validation'
 
 const FormEmployeeRequest = ({ erequest, type, ...others }) => {
   const [data, setData] = useState(erequest)
-  const { onPostExecute, onPutExecute } = useRequestManager()
+  const { onPostExecute, onPutExecute, onGetExecute } = useRequestManager()
   const { showSuccess, showError } = useAlert()
+  const [equi, setEqui] = useState()
 
   const _handleChange = useCallback(
     (field, value) => {
@@ -57,72 +58,106 @@ const FormEmployeeRequest = ({ erequest, type, ...others }) => {
     [data]
   )
 
-  const _renderForm = useCallback(() => {
-    return (
-      <LayoutWrapper>
-        <Form
-          fluid
-          {...others}
-          model={employeeRequestModel}
-          onSubmit={() => onSubmit(data)}
-          formValue={data}
-        >
-          <InputGroup
-            value={withEmpty('equipment_id', data)}
-            label={'Mã thiết bị'}
-            onChange={value => _handleChange('equipment_id', value)}
-            placeholder={'Mã thiết bị'}
-            name={'equipment_id'}
-            leftIcon={<Icon name={'feather-user'} />}
-            require
-          />
-          <InputGroup
-            value={withEmpty('details', data)}
-            label={'Mô tả'}
-            onChange={e => _handleChange('details', e)}
-            placeholder={'Mô tả'}
-            name={'details'}
-            leftIcon={<Icon name={'feather-bookmark'} />}
-            require
-            componentClass='textarea'
-            rows={3}
-          />
-          <InputGroup
-            value={withEmpty('notes', data)}
-            label={'Ghi chú'}
-            onChange={e => _handleChange('notes', e)}
-            placeholder={'Ghi chú'}
-            name={'notes'}
-            leftIcon={<Icon name={'feather-bookmark'} />}
-            require
-            componentClass='textarea'
-            rows={3}
-          />
-          <InputGroup
-            data={datatype}
-            value={withEmpty('type', data)}
-            label={'Hình thức'}
-            onChange={value => _handleChange('type', value)}
-            placeholder={'Hình thức'}
-            name={'type'}
-            leftIcon={<Icon name={'feather-align-justify'} />}
-            placement='autoVerticalStart'
-            accepter={SelectPicker}
-            require
-            block
-            size='sm'
-          />
-          <Wrapper>
-            <Button type={'submit'}>
-              {type == 'update' ? 'Cập nhật' : 'Thêm mới'}
-            </Button>
-          </Wrapper>
-        </Form>
-      </LayoutWrapper>
-    )
-  }, [data])
+  useEffect(() => {
+    async function execute() {
+      const result = await onGetExecute(EndPoint.GET_LIST_EQU, {
+        params: { per_page: 1000 }
+      })
+      if (result) {
+        setEqui(
+          withArray('data', result).map(e => ({
+            ...e,
+            label: `${e.id} ${e.name}`,
+            value: e.id
+          }))
+        )
+      } else {
+        showError('Không lấy được dữ liệu thiết bị')
+      }
+    }
+    execute()
 
-  return _renderForm()
+    return () => {
+      setData({})
+    }
+  }, [])
+
+  return (
+    <LayoutWrapper>
+      <Form
+        fluid
+        {...others}
+        model={employeeRequestModel}
+        onSubmit={() => onSubmit(data)}
+        formValue={data}
+      >
+        <InputGroup
+          data={equi}
+          value={withEmpty('equipment_id', data)}
+          label={'Mã thiết bị'}
+          onChange={value => _handleChange('equipment_id', value)}
+          placeholder={'Mã thiết bị'}
+          name={'equipment_id'}
+          leftIcon={<Icon name={'feather-align-justify'} />}
+          placement='autoVerticalStart'
+          accepter={SelectPicker}
+          require
+          block
+          size='sm'
+        />
+        {/* <InputGroup
+          value={withEmpty('equipment_id', data)}
+          label={'Mã thiết bị'}
+          onChange={value => _handleChange('equipment_id', value)}
+          placeholder={'Mã thiết bị'}
+          name={'equipment_id'}
+          leftIcon={<Icon name={'feather-user'} />}
+          require
+        /> */}
+        <InputGroup
+          value={withEmpty('details', data)}
+          label={'Mô tả'}
+          onChange={e => _handleChange('details', e)}
+          placeholder={'Mô tả'}
+          name={'details'}
+          leftIcon={<Icon name={'feather-bookmark'} />}
+          require
+          componentClass='textarea'
+          rows={3}
+        />
+        <InputGroup
+          value={withEmpty('notes', data)}
+          label={'Ghi chú'}
+          onChange={e => _handleChange('notes', e)}
+          placeholder={'Ghi chú'}
+          name={'notes'}
+          leftIcon={<Icon name={'feather-bookmark'} />}
+          require
+          componentClass='textarea'
+          rows={3}
+        />
+        <InputGroup
+          data={datatype}
+          value={withEmpty('type', data)}
+          label={'Hình thức'}
+          onChange={value => _handleChange('type', value)}
+          placeholder={'Hình thức'}
+          name={'type'}
+          leftIcon={<Icon name={'feather-align-justify'} />}
+          placement='autoVerticalStart'
+          accepter={SelectPicker}
+          require
+          block
+          size='sm'
+        />
+        <Wrapper>
+          <Button type={'submit'}>
+            {type == 'update' ? 'Cập nhật' : 'Thêm mới'}
+          </Button>
+        </Wrapper>
+      </Form>
+    </LayoutWrapper>
+  )
 }
 
 FormEmployeeRequest.propTypes = {
